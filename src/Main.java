36231a6b401af2a33387598781c8b761ef872b90
OrionFLASH <orionflash-msk@yandex.ru>
2025-07-12 19:31:41 +0300

    Recode 1

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,34 @@ public class Main {
     }
 
     public static int countMore(int[] prices, int money) {
+        // Если первый товар уже дороже — все недоступны
+        if (prices[0] > money) {
+            return prices.length;
+        }
+
+        // Если даже последний товар дешевле — все доступны
+        if (prices[prices.length - 1] <= money) {
+            return 0;
+        }
+
         int left = 0;
-        int right = prices.length;
+        int right = prices.length - 1;
 
         while (left < right) {
-            int mid = (left + right) / 2;
-            if (prices[mid] <= money) {
-                left = mid + 1;
-            } else {
-                right = mid;
+            int middle = (left + right) / 2;
+
+            // Если в middle первый недоступный товар,
+            // то все после него тоже недоступны — идем влево
+            if (prices[middle] > money) {
+                right = middle;
+            }
+            // Если товар по цене подходит — ищем правее
+            else {
+                left = middle + 1;
             }
         }
 
-        // left — индекс первого элемента, который > money
+        // После выхода из цикла left — индекс первого недоступного товара
         return prices.length - left;
     }
 }
